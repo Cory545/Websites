a57031152ce4bd8fe592051b4e312b563a6577d1
Cory545 <coryburton545@gmail.com>
2024-05-06 12:50:01 +0100

    Did some basic layout work on Home screen. Made different pages, attached to Nav-Bar.

--- Art Portfolio Website - Commission/tailwind.config.js
+++ Art Portfolio Website - Commission/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./src/**/*.html"],
   theme: {
     extend: {
       backgroundImage: {
-        'background-1' : "url('/Art Portfolio Website - Commission/img/background.png')"
+        'background-1' : "url('/Art Portfolio Website - Commission/img/background.jpg')"
       },
       height: {
         '128': '32rem',
